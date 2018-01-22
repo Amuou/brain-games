aacb2c17db6879ce82786bef98a63d7952e47910
Arthur Faliy <faliyarthur@gmail.com>
2018-01-22 13:11:34 +0200

    integrate eslint and fix lint errors
    	new file:   .eslintrc.yml
            modified:   Makefile
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/bin/brain-games.js
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetingUser = () => {
-    const userName = readlineSync.question('May I have your name?');
-    return `Hello ${userName}!`;
+const greetingUser = () => {
+  const userName = readlineSync.question('May I have your name?');
+  return `Hello ${userName}!`;
 };
+
+export default greetingUser;
